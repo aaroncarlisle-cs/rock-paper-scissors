84ac4fcdcc165b3278bd7c93d8e5eaf94a5114ae
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-15 12:28:08 -0600

    Create functions to get player input and run game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-let gameChoices = ["Rock", "Paper", "Scissors"];
+let gameSelections = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
-    // Randomly return either rock, paper, or scissors as strings 
-    return Math.floor(Math.random()*gameChoices.length);
+    // Randomly return either rock, paper, or scissors as id for gameSelections array
+    return Math.floor(Math.random()*gameSelections.length);
 }
 
 function singleRound(playerSelection, computerSelection) {
     // Function returns 0 for loss, 1 for win, and 2 for tie.
 
+    // User input feedback
+    console.log(`Player chooses ${gameSelections[playerSelection]}`);
+    console.log(`Computer chooses ${gameSelections[computerSelection]}`);
+
     // Check for tie.
     if (playerSelection == computerSelection) {
+        console.log("Tie game!");
         return 2;
     }
     // Get the necessary computer selection for the player to win
@@ -17,10 +22,12 @@ function singleRound(playerSelection, computerSelection) {
 
     // If the computer's choice matches the win condition, player wins.
     if (computerSelection == winCondition) {
+        console.log("Player wins!");
         return 1;
     }
     // Otherwise, player loses.
     else {
+        console.log("Player loses!");
         return 0;
     }
 }
@@ -34,5 +41,39 @@ function getWinCondition(playerSelection) {
             return 0;
         case 2:
             return 1;
+    }
+}
+
+function getPlayerSelection () {
+    let playerSelection = prompt("Please enter rock, paper, or scissors");
+    playerSelection = playerSelection.toLowerCase();
+    let validSelection = false;
+    let selectionID = -1;
+    while (!validSelection) {
+        if (playerSelection == "rock") {
+            selectionID = 0;
+            validSelection = true;
+        }
+        else if (playerSelection == "paper") {
+            selectionID = 1;
+            validSelection = true;
+        }
+        else if (playerSelection == "scissors") {
+            selectionID = 2;
+            validSelection = true;
+        } 
+        else {
+            alert("Invalid selection. Please try again.");
+            playerSelection = prompt("Please enter rock, paper, or scissors");
+            playerSelection = playerSelection.toLowerCase();
+        }
+    }
+    return selectionID;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let result = singleRound(getPlayerSelection(), computerPlay());
+
     }
 }
