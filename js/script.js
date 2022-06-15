59983313b810bed13dde9c7150a1b67c01fe1a81
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-15 11:17:07 -0600

    Code win condition logic for singleRound function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
 let gameChoices = ["Rock", "Paper", "Scissors"];
+
 function computerPlay() {
     // Randomly return either rock, paper, or scissors as strings 
     return Math.floor(Math.random()*gameChoices.length);
 }
+
 function singleRound(playerSelection, computerSelection) {
-    // Strings to be returned, change to 
-    let computerWinsMessage = `${gameChoices[computerSelection]} beats ${gameChoices[playerSelection]}. Computer wins!`;
-    let playerWinsMessage = `${gameChoices[playerSelection]} beats ${gameChoices[computerSelection]}. Player wins!`;
+    // Function returns 0 for loss, 1 for win, and 2 for tie.
+
+    // Check for tie.
+    if (playerSelection == computerSelection) {
+        return 2;
+    }
+    // Get the necessary computer selection for the player to win
+    winCondition = getWinCondition(playerSelection);
+
+    // If the computer's choice matches the win condition, player wins.
+    if (computerSelection == winCondition) {
+        return 1;
+    }
+    // Otherwise, player loses.
+    else {
+        return 0;
+    }
+}
+
+function getWinCondition(playerSelection) {
+    // Set win conditions based on player's choice.
+    switch (playerSelection) {
+        case 0: 
+            return 2;
+        case 1:
+            return 0;
+        case 2:
+            return 1;
+    }
 }
