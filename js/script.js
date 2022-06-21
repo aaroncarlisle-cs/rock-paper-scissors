b586c0f75b0e79fdcdb0f6e38050e9c5cadc5370
Aaron Carlisle <aaroncarlisle.cs@gmail.com>
2022-06-21 10:14:37 -0600

    Restructure updateIcons code to use node objects instead of HTML strings.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,115 @@
 
-const selections = ["Rock", "Paper", "Scissors"];
-const lose = 0;
-const win = 1;
-const draw = 2;
-
 const rock = document.getElementById("rock-button");
 const paper = document.getElementById("paper-button");
 const scissors = document.getElementById("scissors-button");
 const buttons = [rock, paper, scissors];
 
+const lose = 0;
+const win = 1;
+const draw = 2;
+
+
+function game(selection) {
+    let computerSelection = computerPlay();
+    let result = singleRound(selection, computerSelection);
+        if (result == lose) {
+            computerScore++;
+            gameMessage.textContent = "Computer Wins";
+        }
+        else if (result == win) {
+            playerScore++;
+            gameMessage.textContent = "Player Wins";
+        }
+        else if (result == draw) {
+            draws++;
+            gameMessage.textContent = "Draw";
+        }
+        scoreMessage.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+        updateIcons(selection, computerSelection, result);
+}
+
+function computerPlay() {
+    let rand = Math.floor(Math.random()*buttons.length);
+    let computerSelection = buttons[rand];
+    console.log(`Computer chooses ${computerSelection.getAttribute('id')}`);
+    return computerSelection;
+}
+
+function singleRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) return draw;
+    // If not draw, figure out winning combination.
+    winCondition = getWinCondition(playerSelection);
+    if (computerSelection == winCondition) return win;
+    else return lose;
+}
+
+function getWinCondition(playerSelection) {
+    // Returns what computer must choose for player to win
+    switch (playerSelection) {
+        case rock: 
+            return scissors; 
+        case paper: 
+            return rock; 
+        case scissors: 
+            return paper;
+    }
+}
+
+function updateIcons(selection, computerSelection, result) {
+    const icons = document.getElementById("icons");
+    icons.innerHTML = "";
+    let playerIcon = getPlayerIcon(selection);
+    let computerIcon = getComputerIcon(computerSelection);
+    if (result == win) { 
+        playerIcon.classList.add('winner');
+        computerIcon.classList.add('loser');
+    }
+    else if (result == lose) {
+        playerIcon.classList.add('loser');
+        computerIcon.classList.add('winner');
+    }
+    icons.appendChild(playerIcon);
+    icons.appendChild(computerIcon);
+}
+
+function getPlayerIcon(selection) {
+    let playerIcon = document.createElement('i');
+    playerIcon.classList.add('icon', 'fa-10x', 'fa-solid');
+    if (selection == rock) {
+        playerIcon.classList.add('fa-hand-back-fist');
+        playerIcon.setAttribute('data-fa-transform', 'rotate-90');
+        return playerIcon;
+    }
+    if (selection == paper) {
+        playerIcon.classList.add('fa-hand');
+        playerIcon.setAttribute('data-fa-transform', 'rotate-90');
+        return playerIcon;
+    }
+    if (selection == scissors) {
+        playerIcon.classList.add('fa-hand-scissors');
+        playerIcon.setAttribute('data-fa-transform', 'flip-h');
+        return playerIcon;
+    }
+    return;
+}
+function getComputerIcon(computer) {
+    let computerIcon = document.createElement('i');
+    computerIcon.classList.add('icon', 'fa-10x');
+    computerIcon.setAttribute('data-fa-transform', 'rotate-90 flip-h');
+    if (computer == rock) {
+        computerIcon.classList.add('fa-solid', 'fa-hand-fist');
+        return computerIcon;
+    }
+    if (computer == paper) {
+        computerIcon.classList.add('fa-regular', 'fa-hand');
+        return computerIcon;
+    }
+    if (computer == scissors) {
+        computerIcon.classList.add('fa-solid', 'fa-hand-peace');
+        return computerIcon;
+    }
+    return;
+}
 
 let playerScore = 0;
 let computerScore = 0;
@@ -27,107 +128,8 @@ for (button of buttons) {
         this.style.transform = "scale(1)"
     });
     button.addEventListener("click", function () {
-        if (this == rock) game("Rock");
-        if (this == paper) game("Paper");
-        if (this == scissors) game("Scissors");
+        if (this == rock) game(rock);
+        if (this == paper) game(paper);
+        if (this == scissors) game(scissors);
     })
-}
-
-function getPlayerSelection (selection) {
-    playerSelection = selection;
-    return playerSelection;
-}
-
-
-function computerPlay() {
-    let rand = Math.floor(Math.random()*selections.length);
-    let computerSelection = selections[rand];
-    console.log(`Computer chooses ${computerSelection}.`);
-    return computerSelection;
-}
-
-function singleRound(playerSelection, computerSelection) {
-    updateIcons(playerSelection, computerSelection);
-    if (playerSelection == computerSelection) {
-        console.log("Draw!");
-        return draw;
-    }
-    // If not draw, figure out winning combination.
-    winCondition = getWinCondition(playerSelection);
-
-    if (computerSelection == winCondition) {
-        console.log("Player wins!");
-        return win;
-    }
-    else {
-        console.log("Player loses!");
-        return lose;
-    }
-}
-
-function getWinCondition(playerSelection) {
-    // These maintain the game logic if the selection values are renamed in the array
-    let rock = selections[0];
-    let paper = selections[1];
-    let scissors = selections[2];
-
-    // Returns what computer must choose for player to win
-    switch (playerSelection) {
-        case rock: 
-            return scissors; 
-        case paper: 
-            return rock; 
-        case scissors: 
-            return paper;
-    }
-}
-
-function game(selection) {
-    let result = singleRound(selection, computerPlay());
-    // Play i rounds of rock, paper, scissors and reports score.
-        if (result == lose) {
-            computerScore++;
-            gameMessage.textContent = "Computer Wins";
-        }
-        else if (result == win) {
-            playerScore++;
-            gameMessage.textContent = "Player Wins";
-        }
-        else if (result == draw) {
-            draws++;
-            gameMessage.textContent = "Draw";
-        }
-
-        scoreMessage.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
-}
-
-function updateIcons(player, computer) {
-    const icons = document.getElementById("icons");
-    icons.innerHTML = "";
-    icons.innerHTML = getPlayerIcon(player) + getComputerIcon(computer);
-}
-
-function getPlayerIcon(player) {
-    if (player == "Rock") {
-        return '<i class="fa-10x fa-solid fa-hand-back-fist icon" data-fa-transform="rotate-90"></i>';
-    }
-    if (player == "Paper") {
-        return '<i class="fa-10x fa-solid fa-hand icon" data-fa-transform="rotate-90"></i>';
-    }
-    if (player == "Scissors") {
-        return '<i class="fa-10x fa-solid fa-hand-scissors icon" data-fa-transform="flip-h"></i>';
-    }
-    return;
-}
-function getComputerIcon(computer) {
-    if (computer == "Rock") {
-        return '<i class="fa-10x fa-solid fa-hand-fist icon" data-fa-transform="rotate-90 flip-h"></i>';
-    }
-    if (computer == "Paper") {
-        return '<i class="fa-10x fa-regular fa-hand icon" data-fa-transform="rotate-90 flip-h"></i>';
-    }
-    if (computer == "Scissors") {
-        return '<i class="fa-10x fa-solid fa-hand-peace icon" data-fa-transform="rotate-90 flip-h"></i>';
-    }
-    return;
 }
